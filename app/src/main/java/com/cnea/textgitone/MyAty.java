862f2f5b699546b9f58b62b70baadbf3012f95b5
Haveotom <746232495@qq.com>
2017-06-16 14:47:10 +0800

    第二次提交

---
+++ app/src/main/java/com/cnea/textgitone/MyAty.java
@@ -0,0 +1,10 @@
+package com.cnea.textgitone;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/6/16.
+ */
+
+public class MyAty extends AppCompatActivity {
+}
